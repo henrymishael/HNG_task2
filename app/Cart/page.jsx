5f4b92f24aac6b19620fbde0964e9979b84927cd
henrymishael <henrymishael.hm@gmail.com>
2024-07-09 23:26:29 +0100

    fixed button

--- app/Cart/page.jsx
+++ app/Cart/page.jsx
@@ -1,8 +1,12 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import one from "../../public/images/productImages/first1.svg";
+import one from "../../public/images/productImages/second1.svg";
 import { useDispatch, useSelector } from "react-redux";
+// import one from "@/public/images/productImages/second1.svg";
+import two from "../../public/images/productImages/cart.svg";
+import three from "../../public/images/productImages/shoe.svg";
+import four from "../../public/images/productImages/four.svg";
 
 const CartPage = () => {
   const [totalCart, setTotalCart] = useState(0);
@@ -16,7 +20,7 @@ const CartPage = () => {
   }, [cart]);
   return (
     <div className='bg-[#f1f0f0] px-8 py-10 '>
-      <h2 className='text-[32px] font-semibold'>My Cart</h2>
+      <h2 className='text-[32px] font-semibold pb-4'>My Cart</h2>
       <div className='bg-white py-8  px-7 rounded-xl gap-8 flex flex-col'>
         <div className='uppercase flex flex-row items-center justify-between text-lg'>
           <h2>product</h2>
@@ -27,36 +31,108 @@ const CartPage = () => {
             <p>total</p>
           </div>
         </div>
-        {cart.map((cartItem) => (
-          <div
-            key={cartItem.id}
-            className='p-6 dow transition-all duration-200 ease-in-out flex flex-row items-center justify-between'
-          >
-            <div className='flex flex-row items-center gap-6'>
-              <div className='w-[180px] h-[220px]'>
-                <Image src={cartItem.image1} alt='' />
-              </div>
-              <div className=''>
-                <p className='text-[30px] font-bold '>{cart.name}</p>
-                <p className='text-[28px] '>{cartItem.type}</p>
-                <div className='flex items-center gap-2 mt-10'>
-                  <div className='bg-red-900 w-[22px] h-[22px] rounded-full'></div>
-                  <p>Wine</p>
-                </div>
-              </div>
+        {/* {cart.map((cartItem) => ( */}
+        <div className='p-6 dow transition-all duration-200 ease-in-out flex flex-row items-center justify-between'>
+          <div className='flex flex-row items-center gap-6'>
+            <div className='w-[180px] h-[220px]'>
+              <Image src={one} alt='' />
             </div>
-            <div className='flex flex-row items-center justify-between w-[52%]'>
-              <p>M</p>
-              <div className='flex flex-row items-center px-2.5 gap-4 bg-[#f4f4f4] w-20 rounded-[4px]'>
-                <p>-</p>
-                <p className='font-medium'>1</p>
-                <p>+</p>
+            <div className=''>
+              <p className='text-[30px] font-bold '>Lilly</p>
+              <p className='text-[28px] '>Mini bodycon</p>
+              <div className='flex items-center gap-2 mt-10'>
+                <div className='bg-red-900 w-[22px] h-[22px] rounded-full'></div>
+                <p>Wine</p>
               </div>
-              <p>{cartItem.price}</p>
-              <p>100,000</p>
             </div>
           </div>
-        ))}
+          <div className='flex flex-row items-center justify-between w-[52%]'>
+            <p>M</p>
+            <div className='flex flex-row items-center px-2.5 gap-4 bg-[#f4f4f4] w-20 rounded-[4px]'>
+              <p>-</p>
+              <p className='font-medium'>2</p>
+              <p>+</p>
+            </div>
+            <p>50,000</p>
+            <p>100,000</p>
+          </div>
+        </div>
+        <div className='p-6 dow transition-all duration-200 ease-in-out flex flex-row items-center justify-between'>
+          <div className='flex flex-row items-center gap-6'>
+            <div className='w-[180px] h-[220px]'>
+              <Image src={two} alt='' />
+            </div>
+            <div className=''>
+              <p className='text-[30px] font-bold '>Sculpter</p>
+              <p className='text-[28px] '>Silk shaper</p>
+              <div className='flex items-center gap-2 mt-10'>
+                <div className='bg-black w-[22px] h-[22px] rounded-full'></div>
+                <p>Black</p>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-row items-center justify-between w-[52%]'>
+            <p>M</p>
+            <div className='flex flex-row items-center px-2.5 gap-4 bg-[#f4f4f4] w-20 rounded-[4px]'>
+              <p>-</p>
+              <p className='font-medium'>2</p>
+              <p>+</p>
+            </div>
+            <p>22,000</p>
+            <p>22,000</p>
+          </div>
+        </div>
+        <div className='p-6 dow transition-all duration-200 ease-in-out flex flex-row items-center justify-between'>
+          <div className='flex flex-row items-center gap-6'>
+            <div className='w-[180px] h-[220px]'>
+              <Image src={three} alt='' />
+            </div>
+            <div className=''>
+              <p className='text-[30px] font-bold '>Candy</p>
+              <p className='text-[28px] '>Signature pose</p>
+              <div className='flex items-center gap-2 mt-10'>
+                <div className=' bg-pink-400 w-[22px] h-[22px] rounded-full'></div>
+                <p>Pink</p>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-row items-center justify-between w-[52%]'>
+            <p>M</p>
+            <div className='flex flex-row items-center px-2.5 gap-4 bg-[#f4f4f4] w-20 rounded-[4px]'>
+              <p>-</p>
+              <p className='font-medium'>2</p>
+              <p>+</p>
+            </div>
+            <p>50,000</p>
+            <p>100,000</p>
+          </div>
+        </div>
+        <div className='p-6 dow transition-all duration-200 ease-in-out flex flex-row items-center justify-between'>
+          <div className='flex flex-row items-center gap-6'>
+            <div className='w-[180px] h-[220px]'>
+              <Image src={four} alt='' />
+            </div>
+            <div className=''>
+              <p className='text-[30px] font-bold '>Crochetelo</p>
+              <p className='text-[28px] '>Knotted piece</p>
+              <div className='flex items-center gap-2 mt-10'>
+                <div className='bg-red-900 w-[22px] h-[22px] rounded-full'></div>
+                <p>Wine</p>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-row items-center justify-between w-[52%]'>
+            <p>M</p>
+            <div className='flex flex-row items-center px-2.5 gap-4 bg-[#f4f4f4] w-20 rounded-[4px]'>
+              <p>-</p>
+              <p className='font-medium'>2</p>
+              <p>+</p>
+            </div>
+            <p>50,000</p>
+            <p>100,000</p>
+          </div>
+        </div>
+        {/* ))} */}
         <div className='flex flex-col  gap-4 '>
           <p className='text-xl'>CART TOTAL</p>
           <div className='flex items-center justify-between py-2 border-b'>
@@ -65,8 +141,11 @@ const CartPage = () => {
           </div>
           <div className='flex items-center justify-between py-3 border-b'>
             <p>Shipping</p>
-            <p className='text-sm'>
-              Enter your address to view shipping options continue shopping
+            <p className='text-sm w-44'>
+              Enter your address to view shipping options{" "}
+              <span className='underline-offset-1 text-primary underline'>
+                Calculate shopping
+              </span>
             </p>
           </div>
           <div className='flex items-center pb-4 border-b'>
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -11,6 +11,17 @@ import { addToCart, removeFromCart } from "@/store/slices/cart-slice";
 const ProductCard = ({ product }) => {
   // const [cartItem, setCartItem] = useRecoilState(cartState);
   const [buttonText, setButtonText] = useState("Add to Cart");
+  const [quantity, setQuantity] = useState(1);
+
+  const incrementQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
 
   const dispatch = useDispatch();
   const { cart } = useSelector((state) => state);
@@ -54,9 +65,13 @@ const ProductCard = ({ product }) => {
       </div>
       <div className='flex items-center justify-between py-2'>
         <div className='xl:rounded-sm rounded-[3px] md:px-1.5 md:gap-4 gap-2.5  text-[12px]  flex flex-row items-center bg-white xl:py-4  md:p-2 xsm:px-[8px]  lg:px-2.5 h-6 '>
-          <p>-</p>
-          <p className='font-medium'>{product.quantity}</p>
-          <p>+</p>
+          <p className='cursor-pointer' onClick={decrementQuantity}>
+            -
+          </p>
+          <p className='font-medium'>{quantity}</p>
+          <p className='cursor-pointer' onClick={incrementQuantity}>
+            +
+          </p>
         </div>
 
         <button
